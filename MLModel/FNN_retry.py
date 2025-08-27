438323a73faf5234c92fa5ff48761f939832b6cd
Sadra <sadra.f.001@gmail.com>
2025-08-27 19:02:49 +0330

    added a and z lists of node outputs.

--- MLModel/FNN_retry.py
+++ MLModel/FNN_retry.py
@@ -4,10 +4,14 @@ import numpy as np
 
 
 class FeedForwardNeuralNetwork:
+    """
+    A Classifier Feed Forward Neural Network Built From Scratch With Numpy.
+    """
     def __init__(self):
         self.structure = None
-        self.a = []
         self.z = []
+        self.a = []
+        # self.batch_size = 100
 
     def build(self, inp, hidden:list[list], outp):
         self.layout = [inp] + hidden + [outp]
@@ -26,4 +30,20 @@ class FeedForwardNeuralNetwork:
         self.structure.append([[f"O{j}-W{w}" for w in range(self.layout[-2] + 1)] for j in range(self.layout[-1])])
         self._weight_template.append([np.random.rand(self.layout[-2] + 1) for j in range(self.layout[-1])])
 
-    
+    def train(self, X, Y):
+        self.weights = self._weight_template.copy()
+        self.z = []
+        self.a = []
+        for _, iv in enumerate(self.layout[1:]):
+            self.z.append([None for _ in range(iv)])
+            self.a.append([None for _ in range(iv)])
+        for v in X:
+            self._forward(v)
+
+    def _forward(self, x):
+        _x = np.insert(x, 0, 1)
+        for i, iv in enumerate(self.layout[1:]):
+            # self.z[]
+            pass
+
+        
