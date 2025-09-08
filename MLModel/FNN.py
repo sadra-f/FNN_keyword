5bc5f39f750e60c981873866e21b587dfeb3fa9d
Sadra <sadra.f.001@gmail.com>
2025-09-08 14:43:43 +0330

    did some cleaning and commenting.

--- MLModel/FNN.py
+++ MLModel/FNN.py
@@ -9,11 +9,18 @@ class FeedForwardNeuralNetwork:
     """
     def __init__(self):
         self.structure = None
+        self._z_template = []
+        self._a_template = []
+        self._a_template = []
+        
         self.z = None
         self.a = None
         self._errors = None
+        
         self.gradients = None
+        
         self.loss_hist = None
+        
         self.RELU_CONST = 0.01
         self.LEARNING_RATE = 0.001
         # self.batch_size = 100
@@ -21,45 +28,63 @@ class FeedForwardNeuralNetwork:
     def build(self, inp, hidden:list[list], outp):
         if outp < 2:
             raise ValueError("The NN is designed as to process binary classification as 2 class classification so outp must be >=2")
+        # first, input layer
         self.layout = [inp] + hidden + [outp]
         self.structure = [[f"I{i}" for i in range(self.layout[0])]]
         self._weight_template = []
+        # hidden layers
         for i in range(1, len(self.layout)-1, 1):
             tmp = []
             tmp_w = []
+            # layer nodes
             for j in range(self.layout[i]):
                 # +1 in range for the bias
                 tmp.append([f"L{i}-N{j}-W{w}" for w in range(self.layout[i-1] + 1)])
                 std_dev = np.sqrt(2.0 / (self.layout[i-1] + 1))
+                # node weights
                 tmp_w.append(np.random.normal(loc=0.0, scale=std_dev, size=self.layout[i-1]+1))
             self.structure.append(tmp)
             self._weight_template.append(tmp_w)
+        #final, output layer
         # +1 in range for the bias
         self.structure.append([[f"O{j}-W{w}" for w in range(self.layout[-2] + 1)] for j in range(self.layout[-1])])
         std_dev = np.sqrt(2.0 / (self.layout[-1] + 1))
         self._weight_template.append(np.random.normal(loc=0.0, scale=std_dev, size=(self.layout[-1], self.layout[-2]+1)))
+        # build templates for other parameters
+        self._z_template = []
+        self._a_template = []
+        self._errors_template = []
+        for _, iv in enumerate(self.layout[1:]):
+            self._z_template.append([None for _ in range(iv)])
+            self._a_template.append([None for _ in range(iv)])
+            self._errors_template.append([None for _ in range(iv)])
 
     def train(self, X, Y, epoch=1):
+        #initialize parameters
         self.weights = copy.deepcopy(self._weight_template)
-        self.z = []
-        self.a = []
-        self._errors = []
-        self.gradients = []
+
         self.loss_hist = []
-        for _, iv in enumerate(self.layout[1:]):
-            self.z.append([None for _ in range(iv)])
-            self.a.append([None for _ in range(iv)])
-            self._errors.append([None for _ in range(iv)])
-            self.gradients = copy.deepcopy(self._weight_template)
+        
+        self.z = copy.deepcopy(self._z_template)
+        self.a = copy.deepcopy(self._a_template)
+        self._errors = copy.deepcopy(self._errors_template)
+
+        self.gradients = copy.deepcopy(self._weight_template)
+        # train the model
         for epc in range(epoch):
             self.loss_hist.append([])
             for i, _x in enumerate(X):
                 y = self._onehot_encode(Y[i])
                 x = self._normalize(_x)
+
                 self._forward(x)
+
                 self.loss_hist[epc].append(self.cross_entropy(y, self.a[-1]))
+                
+                # backpropagation
                 self._err(y)
                 self._clac_gradient(x)
+
                 self._update_weights()
 
     def _forward(self, x):
@@ -81,20 +106,26 @@ class FeedForwardNeuralNetwork:
 
     def predict(self, x, logits=False):
         _x = np.insert(self._normalize(x), 0, 1)
+        # make a deepcopy and overwrite since they have the same structure for training and testing
         tmp_z = copy.deepcopy(self.z)
         tmp_a = copy.deepcopy(self.a)
+        # first hiddel layer
         for j in range(self.layout[1]):
             tmp_z[0][j] = np.sum(self.weights[0][j] * _x)
             tmp_a[0][j] = self._leaky_relu(tmp_z[0][j])
+        # rest of hidden layers
         for i in range(1, len(tmp_z)-1, 1):
             for j in range(len(tmp_z[i])):
                 tmp_z[i][j] = np.sum(self.weights[i][j] * np.insert(tmp_a[i-1], 0, 1))
             tmp_a[i] = self._leaky_relu(tmp_z[i])
+        # final, output layer
         for j in range(len(tmp_z[-1])):
             tmp_z[-1][j] = np.sum(self.weights[-1][j] * np.insert(tmp_a[-2], 0, 1))
         tmp_a[-1] = self.soft_max(tmp_z[-1])
+
         if logits:
             return (np.array(tmp_a[-1]).argmax(), tmp_a)
+        
         return np.array(tmp_a[-1]).argmax()
 
     def soft_max(self, x):
